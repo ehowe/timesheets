1e783cf04d8e948fa7cc4ec876b9cfa609bfb252
Eugene Howe <eugene.howe@protonmail.com>
2021-08-25 21:04:17 -0400

    fix specs

--- spec/requests/timesheet_entries_spec.rb
+++ spec/requests/timesheet_entries_spec.rb
@@ -3,8 +3,8 @@ require_relative "../rails_helper"
 describe "Timesheet Entry requests" do
   let(:timesheet)    { create(:timesheet, user: user) }
   let(:category)     { create(:payroll_category) }
-  let(:start_at)     { Time.new(2021, 8, 1, 6, 30, 0, "-04:00").iso8601 }
-  let(:end_at)       { Time.new(2021, 8, 1, 18, 30, 0, "-04:00").iso8601 }
+  let(:start_at)     { Time.new(2021, 8, 9, 6, 30, 0, "-04:00").iso8601 }
+  let(:end_at)       { Time.new(2021, 8, 9, 18, 30, 0, "-04:00").iso8601 }
   let(:valid_params) { { entry: { payroll_category_id: category.id, start_at: start_at, end_at: end_at }, token: user.token } }
   let(:path)         { "/api/timesheets/#{timesheet.id}/entries" }
   let(:user)         { create(:user) }
